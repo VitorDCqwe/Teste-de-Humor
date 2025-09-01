e5b9ca3fca3a3e2e4b3358313cf53399cb3c9832
Vitor Duarte Chaves <vitord.chaves@gmail.com>
2025-09-01 14:58:49 -0300

    Atualização JavaScript

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ Webcan.attach('#camera');
 
 function takeSnapshot() {
     Webcan.snap(function (datauri) {
-        document.getElementById("result").innerHTML = `<img id="captureImage" src="${datauri}">`;
+        document.getElementById("result").innerHTML = `<img id="capturedImage" src="${datauri}">`;
     });
 }
 
@@ -30,4 +30,32 @@ function speak() {
     let synth = window.speechSynthesis
     let speakData1 = "A primeira previsão é " + prediction1;
     let speakData2 = "E a segunda previsão é " + prediction2;
+    let utterThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
+    synth.speak(utterThis);
+}
+
+function check(classifier) {
+    let img = document.getElementById('capturedImage');
+    if(!classifier) {
+        console.error("O modelo não foi carregado!");
+        return;
+    }
+    classifier.classify(img)
+    .then(result => {
+        console.log(results);
+        document.getElementById("resultEmotionName").innerHTML = result[0].label;
+        document.getElementById("resultEmotionName2").innerHTML = result[1].label;
+        prediction1 = result[0].label;
+        prediction2 = result[1].label;
+        speak();
+        if(results[0].label === "feliz") {
+            document.getElementById("updateEmoji").innerHTML = "&#128522;";
+        }
+        if(results[0].label === "triste") {
+            document.getElementById("updateEmoji").innerHTML = "&#128532;";
+        }
+        if(results[0].label === "irritado") {
+            document.getElementById("updateEmoji").innerHTML = "&#128548;";
+        }
+    })
 }
