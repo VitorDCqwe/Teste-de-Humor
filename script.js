ab7f2bfc72c6370d50d632413e389a13b96932e7
Vitor Duarte Chaves <vitord.chaves@gmail.com>
2025-09-06 09:42:13 -0300

    finalização

--- script.js
+++ script.js
@@ -57,5 +57,15 @@ function check(classifier) {
         if(results[0].label === "irritado") {
             document.getElementById("updateEmoji").innerHTML = "&#128548;";
         }
+        if(results[1].label === "feliz") {
+            document.getElementById("updateEmoji2").innerHTML = "&#128522;";
+        }
+        if(results[1].label === "triste") {
+            document.getElementById("updateEmoji2").innerHTML = "&#128532;";
+        }
+        if(results[1].label === "irritado") {
+            document.getElementById("updateEmoji2").innerHTML = "&#128548;";
+        }
     })
+    .catch(err => console.error(err));
 }
